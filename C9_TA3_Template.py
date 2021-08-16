2ae850722fa305b06f47009d436ca88f1234a43f
Anjali140887 <86819899+Anjali140887@users.noreply.github.com>
2021-08-16 11:31:01 +0530

    Add files via upload

---
+++ C9_TA3_Template.py
@@ -0,0 +1,9 @@
+import pygame,sys,random
+pygame.init()
+clock=pygame.time.Clock()
+pygame.display.set_mode((400,600))
+pygame.display.set_caption('Bird Game')
+while True:
+    screen.fill((50,150,255))
+    screen.blit(images["bg1"],[0,0])
+    pygame.quit()
