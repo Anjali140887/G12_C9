d615ba832eb5b926d44ecf28dbf20f3042814c96
Anjali140887 <86819899+Anjali140887@users.noreply.github.com>
2021-08-16 12:53:21 +0530

    Update C9_TA3_Template.py

--- C9_TA3_Template.py
+++ C9_TA3_Template.py
@@ -4,6 +4,5 @@ clock=pygame.time.Clock()
 pygame.display.set_mode((400,600))
 pygame.display.set_caption('Bird Game')
 while True:
-    screen.fill((50,150,255))
     screen.blit(images["bg1"],[0,0])
     pygame.quit()
