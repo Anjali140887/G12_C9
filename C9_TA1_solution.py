85ad994e7fe2587ea2c3350d04b6519845da3020
Anjali140887 <86819899+Anjali140887@users.noreply.github.com>
2021-08-29 17:29:19 +0530

    Update C9_TA1_solution.py

--- C9_TA1_solution.py
+++ C9_TA1_solution.py
@@ -2,6 +2,6 @@ import pygame,sys,random
 pygame.init()
 clock=pygame.time.Clock()
 pygame.display.set_mode((400,600))
-pygame.display.set_caption('Bird Game')
+pygame.display.set_caption('Infinite Flying Bird Game')
 while True:
   pygame.quit()
